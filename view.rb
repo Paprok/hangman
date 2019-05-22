dc6beedf87b0119658e6899eb74f7fba4814bc11
Paprok <rafal.becla@gmail.com>
2019-05-22 20:34:18 +0200

    displays blank word to guess

--- view.rb
+++ view.rb
@@ -4,7 +4,20 @@ class View
     @country_and_capital = country_and_capital
   end
 
-  def show_word
-    # code here
+  def show_blank(blank)
+    puts 'GUESS THE CAPITAL!'
+    to_show = ''
+    size = blank.size
+    size -= 1
+    (0..size).each {|i| add_letter_and_space(blank, i, to_show)
+    }
+    puts to_show
+  end
+
+  private
+
+  def add_letter_and_space(blank, i, to_show)
+    to_show << blank[i]
+    to_show << ' '
   end
 end
