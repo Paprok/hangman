f782d5cf7b078795245e1b1c6fe2f21c21eb2d33
Paprok <rafal.becla@gmail.com>
2019-05-22 20:35:44 +0200

    proper for range and method name change

--- game.rb
+++ game.rb
@@ -6,13 +6,13 @@ class Game
   def initialize(lives)
     @lives = lives
     @country_and_capital = get_country_and_capital
-    @guessed_capital = create_blank
+    @blank = create_blank
     @view = View.new(@country_and_capital)
   end
 
   def run
     while @lives > 0
-      @view.show_word
+      @view.show_blank(@blank)
       @lives -= 1;
     end
   end
@@ -23,12 +23,14 @@ class Game
     coutries_and_capitols = FileLoader.new('country_and_capital.txt').countries_and_capitols
     n = rand(coutries_and_capitols.length)
     capital_and_country = coutries_and_capitols[n]
+    puts capital_and_country    #only for testing purposes
+    return capital_and_country
   end
 
   def create_blank
     length = @country_and_capital[1].size
     blank = ''
-    for i in 1..length
+    for i in 0..length
       blank << '_'
     end
     return blank
