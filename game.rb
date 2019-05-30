d0ac22f758abe29d2e70f7aa5aaa094a350286f7
Paprok <rafal.becla@gmail.com>
2019-05-30 10:20:59 +0200

    refactored get_country_and_capital to use CountryCapital objects, clean coded run and create_blank

--- game.rb
+++ game.rb
@@ -6,34 +6,41 @@ class Game
   def initialize(lives)
     @lives = lives
     @country_and_capital = get_country_and_capital
-    @blank = create_blank
+    @blank = create_blank(@country_and_capital.capital)
     @view = View.new(@country_and_capital)
   end
 
   def run
-    while @lives > 0
-      @view.show_blank(@blank)
-      @lives -= 1;
+    while @lives.positive?
+      guess
     end
   end
 
   private
 
+  def guess
+    @view.show_blank(@blank)
+    @lives -= 1
+  end
+
   def get_country_and_capital
-    coutries_and_capitols = FileLoader.new('country_and_capital.txt').countries_and_capitols
-    n = rand(coutries_and_capitols.length)
-    capital_and_country = coutries_and_capitols[n]
-    puts capital_and_country    #only for testing purposes
-    return capital_and_country
+    countries_and_capitals = FileLoader.new('country_and_capital.txt').countries_and_capitals
+    n = rand(countries_and_capitals.length)
+    capital_and_country = countries_and_capitals[n]
+    puts "cheat: #{capital_and_country}" #only for testing purposes
+    capital_and_country
   end
 
-  def create_blank
-    length = @country_and_capital[1].size
+
+  def create_blank(capital)
+    length = capital.size
+    length -= 1
     blank = ''
-    for i in 0..length
-      blank << '_'
-    end
-    return blank
+    (0..length).each { |i| blank << get_blank(capital, i) }
+    blank
   end
 
+  def get_blank(capital, i)
+    capital[i] == ' ' ? ' ' : '_'
+  end
 end
