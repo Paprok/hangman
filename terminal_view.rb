af324378635e4a8f1e77081a7b9402e3ad5c888c
Paprok <rafal.becla@gmail.com>
2019-05-31 17:18:42 +0200

    renamed view to terminal_view, added docs

--- terminal_view.rb
+++ terminal_view.rb
@@ -1,10 +1,17 @@
-class View
+##
+# Class serves as terminal implementation of view, gets input and
+# prints status messages to terminal
+class TerminalView
 
+  ##
+  # shows message with/without hint based on +game_data+
   def guess_message(game_data)
     message = 'GUESS THE CAPITAL'
     game_data.lives == game_data.hint_at ? puts("#{message} OF #{game_data.country}") : puts("#{message}!")
   end
 
+  ##
+  # shows in terminal current state of +blank+ (word to be guess)
   def show_blank(blank)
     to_show = ''
     size = blank.size - 1
@@ -12,10 +19,14 @@ class View
     puts to_show
   end
 
+  ##
+  # shows +lives+ left in terminal
   def show_lives(lives)
     puts "#{lives} Lives left"
   end
 
+  ##
+  # gets input from user untill consist of only letters and/or spaces
   def get_guess
     puts('type letter or whole capital:')
     guessed = gets.chomp
@@ -26,21 +37,37 @@ class View
     guessed.upcase
   end
 
+  ##
+  # shows +wrongly_guessed+ - letters/words previously wrongly guessed
+  # in comma separated format
   def show_wrongly_guessed(wrongly_guessed)
-    guesses = ""
-    wrongly_guessed.each {|guess| guesses += ", #{guess}"}
+    guesses = ''
+    wrongly_guessed.each {|guess| guesses += "#{guess}, "}
+    guesses = validate_guesses(guesses)
     puts("Guessed so far: #{guesses}")
   end
 
+  ##
+  # shows loss message in terminal
   def announce_loss
     puts 'You lost, better luck next time!'
   end
 
+  ##
+  # shows win message in terminal
   def announce_win(game_data)
     puts "You won with #{game_data.lives} left!"
   end
 
   private
+
+  def validate_guesses(guesses)
+    guesses = guesses.strip
+    if guesses.size > 1
+      guesses = guesses[0...-1]
+    end
+    guesses
+  end
 
   def add_letter_and_space(blank, i, to_show)
     to_show << blank[i]
